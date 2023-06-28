b7f588bb75f4eec1bc55b7d9b630221c14439ea4
FilippoBotti <filippo.botti.itis@gmail.com>
2023-06-28 11:32:22 +0200

    Added comments

--- jms/src/main/java/it/unipr/botti/jms/Node.java
+++ jms/src/main/java/it/unipr/botti/jms/Node.java
@@ -60,11 +60,21 @@ public class Node {
         return this.id;
     }
 
+    /*
+     * This method simulates the resource's utilization.
+     * 
+     * @param int delta
+     * 
+     * It accept one integer which will be used to determinate the execution time.
+     * It is also in charge to send ping messages to the coordinator in order to prevent resource's revokation from the coordinator
+     * After the execution time it will sleep the node for a random amount of time in range of 5 seconds.
+     * 
+     */
     public void consumeResource(int delta){
         if(System.currentTimeMillis() - this.prevMillisPing >= PING_RATE_TIME){
             sender.sendResourcesMessage(this.coordinatorId, MessageType.PING_RESOURCE);
         }
-        if(System.currentTimeMillis() -this.initResoruceTime >=DEFAULT_EXECUTION_TIME*delta){
+        if(System.currentTimeMillis() - this.initResoruceTime >=DEFAULT_EXECUTION_TIME*delta){
             this.setState(State.EXECUTOR_IDLE);
             sender.sendResourcesMessage(this.coordinatorId, MessageType.FREE_RESOURCE);
             System.out.println("\uD83C\uDD93 Resource consumed. My state is: " + this.getState());
@@ -74,14 +84,26 @@ public class Node {
         }
     }
 
+    /*
+     * This method is in charge to verify if the node which has the resource is still awake.
+     * It verifies if the last ping message has been received in the corrrect interval time
+     * If not it will be consider the node dead and it will rewoke the resource
+     */
     public void checkResourceState(){
-        if(System.currentTimeMillis()-this.lastPingReceived >=PING_CHECK_RATE_TIME){
+        if(System.currentTimeMillis()-this.lastPingReceived >= PING_CHECK_RATE_TIME){
             System.out.println("Executor off, resource will be consider free from now");
             this.nodeWithResource=-1;
             this.resource= ResourceState.FREE;
         }
     }
 
+    /*
+     * This method provides the state changes.
+     * If a node is awake and the random probability is less than K it will put the node in the dead state and 
+     * it will sleep the node for 5 seconds.
+     * Else if a node is dead and the random probability is less than H+K it will wake the node up (flush the queue messages) and it will start new election phase,
+     * with the node in the candidate state
+     */
     public void changeState() throws JMSException {
         int randomInt = this.random.nextInt(MAX - MIN) + MIN;
 
@@ -106,6 +128,11 @@ public class Node {
         
     }
 
+    /*
+     * This method randomly chooses to ask the coordinator for the resource.
+     * It puts the node in the waiting_for_resource state if the random probability is less than 50 and it will send an ask_for_resource message to the coordinator
+     * Else it will sleep the node for a random amount of time (in range of 5 seconds)
+     */
     public void askForResource() {
         int randomInt = random.nextInt(MAX - MIN) + MIN;
         if (randomInt < 50)
@@ -121,6 +148,11 @@ public class Node {
         }
     }
 
+    /*
+     * This methods sleeps the node for delta seconds
+     * 
+     * @param int delta
+     */
     public void sleeping(int delta){
         try{
             Thread.sleep(delta*DEFAULT_WAIT_TIME);
@@ -130,9 +162,21 @@ public class Node {
         }
     }
 
+    /*
+     * This method provides bully's algorithm implementation for the election phase.
+     * If the node is candidate it will send an election messages to all the nodes with id greater than the current node id.
+     * Then, until the coordinator is not elected, it will wait for response messages:
+     * If the message's type is election it will send an acknowledgment to the node which has send the message, and send a message to all of the 
+     * nodes with id greater than the current node id.
+     * If it's an acknowledgment it will wait for the coordinator.
+     * If it's a new_coordinator_messages it will put the node in the executor state, but only if the node was waiting for the coordinator (cause if it's not
+     * it means that the node didn't receive any acknowledgment, so the election is not correct and new election will be required)
+     * If it's an election_timeout_message and the node was waiting for the coordinator it will wait for another message (it means that the election isn't done),
+     * else it means that the node has not receive any acknowledgment so the node is the coordinator and it will send new_coordinator_message to all of the other nodes
+     */
+
     public void election(){
         try{
-            //booleano: se ricevo almeno un acknowledgment aspetto la comunicazione del coordinatore
             CustomMessage receivedMessage;
             System.out.println("Election phase");
             if(this.getState()==State.CANDIDATE){
@@ -185,8 +229,25 @@ public class Node {
         }
     }
 
+    /*
+     * This method provides the execution phase.
+     * First if the node invoke tha change state methods to randomly change the state
+     * Then if the node has the resource it will consume it with the respective method
+     * If the node it the coordinator and the resource is assigned it will invoke the method to check the resource state with ping messages
+     * If the node is an executor without the resource it will invoke the metode to randomly ask for the resource.
+     * 
+     * If the node is not dead it will wait for the message:
+     * If the message is an ask for the resource and the node is the coordinator it will check the resource state and assign it if it's free
+     * If the message is a new coordinator it means that a new election has been done by a node with a greather id so it simply change the state to executor
+     * If the message is election it means that new election has been initiate and it will invoke the election phase (also for the timeout_election message)
+     * If the message is timeout resource and the node was waiting for the resource it will invoke new election cause it means that the coordinator is dead
+     * If the message is free resource it will revoke the resource from the node (it means that the node has consumed the resource)
+     * If the message is resource acknownledgment it means that the node can get the resource, if it's resource_busy it means that the resource is already assigned to another node
+     */
     public void executionPhase() throws JMSException{
         while(true){
+            changeState();
+
             if(this.getState()==State.EXECUTOR_WITH_RESOURCE){
                 int randomInt = random.nextInt(3) + 1;
                 consumeResource(randomInt);
@@ -195,7 +256,7 @@ public class Node {
             if(this.getState()==State.COORDINATOR && this.getResourceState()==ResourceState.BUSY){
                 checkResourceState();
             }
-            changeState();
+            
             if(this.getState()!= State.COORDINATOR && this.getState()!=State.DEAD && this.getState()!=State.EXECUTOR_WITH_RESOURCE){
                 askForResource();
             }
@@ -305,7 +366,7 @@ public class Node {
         System.out.println("Initializing node ...");
         n.sender.createSenderQueue();
         n.receiver.createReceiverQueue();
-        System.out.println("\n\n");
+        System.out.println("\n");
         try{
             Thread.sleep(4000);
         } catch (Exception e){
